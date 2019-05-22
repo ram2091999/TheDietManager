2fa593384ba7479b30558c0a3e8e11556c799299
ram2091999 <43640116+ram2091999@users.noreply.github.com>
2019-05-22 16:21:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -225,3 +225,12 @@ document.getElementsByClassName("resetButton")[0].addEventListener("click",funct
   localStorage.clear();
   document.location.reload(true);
 });
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
