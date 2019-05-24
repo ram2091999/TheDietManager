740675d070f98886410a374e4bbea19aef142979
ram2091999 <43640116+ram2091999@users.noreply.github.com>
2019-05-24 16:33:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ document.getElementsByClassName("resetButton")[0].addEventListener("click",funct
 function isNumber(evt) {
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    if ((charCode > 31 && (charCode < 48 || charCode > 57))||charCode!=190) {
         return false;
     }
     return true;
